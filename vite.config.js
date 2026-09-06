c19c06899424f7b02708db4c4dcfc876d68c8abd
OmarAhmed <omarahmedgodabmox@gmail.com>
2026-09-06 11:14:40 +0300

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test-crudsystem/',
+  base: '/',
 	plugins: [react(), tailwindcss()],
 });
